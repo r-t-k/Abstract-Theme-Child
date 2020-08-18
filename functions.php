9c5ca64015773bd7f2e00c51e8fd938fd2cc30f2
Tommy <tommy@kyser.io>
2020-08-17 19:14:28 -0500

    bump

--- functions.php
+++ functions.php
@@ -2,11 +2,11 @@
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
-/*ini_set('display_errors', 1);
+ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-ini_set('xdebug.var_display_max_data', '-1');*/
+ini_set('xdebug.var_display_max_data', '-1');
 /* ================================================================================================ */
 /*                                  WP Plugin Update Server                                         */
 /* ================================================================================================ */
